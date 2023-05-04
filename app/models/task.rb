8658cc1f3aab0449aa70e0654b103cafc8702282
Luan Tedesco <luanfelipe2040@gmail.com>
2023-05-04 13:56:48 -0300

    added view today

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+
+  scope :today, -> { where(end_date: Date.today) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'today', to: 'today#index', as: :today
 end
