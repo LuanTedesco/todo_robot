d7d08c3f71940c35150bf714dc7b8f0e457435aa
Luan Tedesco <luanfelipe2040@gmail.com>
2023-05-11 10:39:32 -0300

    fix filter to use one controller and one view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
   resources :tasks
-
   root to: redirect('/tasks')
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  get 'today', to: 'today#index', as: :today
-  get 'ended', to: 'ended#index', as: :ended
 end
