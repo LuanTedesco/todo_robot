a8c79ca09e032d05f984f14196739fb974149641
Luan Tedesco <luanfelipe2040@gmail.com>
2023-05-15 16:21:20 -0300

    added relationship table task to priority on model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ApplicationRecord
   scope :today, -> { where('start_date <= ? AND end_date >= ?', Date.today, Date.today) }
   scope :today_ended, -> { where(finished: false) }
   scope :ended, -> { where(finished: true) }
-
   validates :title, presence: true
+  belongs_to :priority
 end
