195c8df22a9e1b117e2527c40996798247f83282
LuanTedesco <luanfelipe2040@gmail.com>
2023-05-29 22:26:12 -0300

    added more filters

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,13 +2,39 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
 
   def index
-    @tasks = if params[:filter] == 'today'
-               Task.today.today_ended
-             elsif params[:filter] == 'ended'
-               Task.ended
+    filter
+    filter_index
+  end
+
+  def filter
+    @tasks = if params[:filter] == 'today_tasks'
+               Task.today.today_ended.task
+             elsif params[:filter] == 'ended_tasks'
+               Task.ended.task
+             elsif params[:filter] == 'tasks'
+               Task.task
+             elsif params[:filter] == 'today_robots'
+               Task.today.today_ended.robot
+             elsif params[:filter] == 'ended_robots'
+               Task.ended.robot
+             elsif params[:filter] == 'robots'
+               Task.robot
+             elsif params[:filter] == 'aguardando'
+               Task.aguardando
+             elsif params[:filter] == 'desenvolvendo'
+               Task.desenvolvendo
+             elsif params[:filter] == 'testando'
+               Task.testando
+             elsif params[:filter] == 'concluido'
+               Task.concluido
+             elsif params[:filter] == 'rodando'
+               Task.rodando
              else
                Task.all
              end
+  end
+
+  def filter_index
     if params[:filter_title].present?
       @tasks = @tasks.where('LOWER(title) LIKE ?',
                             "%#{params[:filter_title].downcase}%")
@@ -20,57 +46,57 @@ class TasksController < ApplicationController
     @tasks = @tasks.where(typetask: params[:filter_typetask]) if params[:filter_typetask].present?
     @tasks = @tasks.order(priority: :desc, finished: :desc)
   end
+end
 
-  def show; end
+def show; end
 
-  def new
-    @task = Task.new
+def new
+  @task = Task.new
+end
+
+def edit; end
+
+def create
+  @task = Task.new(task_params)
+  if @task.save
+    redirect_to tasks_path, notice: 'Tarefa criada com sucesso!'
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
-  def edit; end
+def fast_task
+  @task = Task.new(task_params)
+  @task.start_date = Date.today
+  @task.end_date = Date.today
 
-  def create
-    @task = Task.new(task_params)
-    if @task.save
-      redirect_to tasks_path, notice: 'Tarefa criada com sucesso!'
-    else
-      render :new, status: :unprocessable_entity
-    end
+  if @task.save
+    redirect_to tasks_path, notice: 'Tarefa criada com sucesso!'
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
-  def fast_task
-    @task = Task.new(task_params)
-    @task.start_date = Date.today
-    @task.end_date = Date.today
-
-    if @task.save
-      redirect_to tasks_path, notice: 'Tarefa criada com sucesso!'
-    else
-      render :new, status: :unprocessable_entity
-    end
+def update
+  if @task.update(task_params)
+    redirect_to tasks_path, notice: 'Tarefa alterada com sucesso!'
+  else
+    render :edit, status: :unprocessable_entity
   end
+end
 
-  def update
-    if @task.update(task_params)
-      redirect_to tasks_path, notice: 'Tarefa alterada com sucesso!'
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @task.destroy
-    redirect_to tasks_path, notice: 'Tarefa excluída com sucesso!'
-  end
+def destroy
+  @task.destroy
+  redirect_to tasks_path, notice: 'Tarefa excluída com sucesso!'
+end
 
   private
 
-  def set_task
-    @task = Task.find(params[:id])
-  end
+def set_task
+  @task = Task.find(params[:id])
+end
 
-  def task_params
-    params.require(:task).permit(:title, :description, :start_date, :end_date, :result, :priority, :status, :typetask,
-                                 :finished)
-  end
+def task_params
+  params.require(:task).permit(:title, :description, :start_date, :end_date, :result, :priority, :status, :typetask,
+                               :finished)
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,13 @@ class Task < ApplicationRecord
   scope :today, -> { where('start_date <= ? AND end_date >= ?', Date.today, Date.today) }
   scope :today_ended, -> { where(finished: false) }
   scope :ended, -> { where(finished: true) }
+  scope :task, -> { where(typetask: 'Tarefa') }
+  scope :robot, -> { where(typetask: 'Robô') }
+  scope :aguardando, -> { where(priority: 'Aguardando') }
+  scope :desenvolvendo, -> { where(priority: 'Desenvolvendo') }
+  scope :testando, -> { where(priority: 'Testando') }
+  scope :concluido, -> { where(priority: 'Concluído') }
+  scope :rodando, -> { where(priority: 'Rodando') }
   validates :title, presence: true
 
   enum priority: {
