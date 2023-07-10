781c94881da3427019ddab81b2e6e9149ca0a459
Luan Tedesco <luanfelipe2040@gmail.com>
2023-07-10 16:45:53 -0300

    changed modal

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,11 +4,8 @@
 
 import { application } from './application'
 
-import HelloController from './hello_controller'
-application.register('hello', HelloController)
-
 import ModalController from './modal_controller'
 application.register('modal', ModalController)
 
-import TaskModalController from './task_modal_controller'
-application.register('task-modal', TaskModalController)
+import TaskModalController from './generic_modal_controller'
+application.register('generic-modal', TaskModalController)
