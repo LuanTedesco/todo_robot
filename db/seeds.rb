3791c648f67edc983e75b7a8dea9ef50f885ed29
Luan Tedesco <luanfelipe2040@gmail.com>
2023-10-09 16:44:30 -0300

    addes seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'admin@admin.com', password: '123456',  password_confirmation: '123456')
+
+Tag.create(id: 1, name: 'Fiscal', user_id: 1)
+Tag.create(id: 2, name: 'Rh', user_id: 1)
+Tag.create(id: 3, name: 'Societario', user_id: 1)
+
+Task.create(title: 'Tarefa 1', description: 'Tarefa 1', start_date: Date.today, end_date: Date.today, priority: :low, status: :waiting, typetask: :task, finished: false, tag_ids: [1,2,3], user_id: 1)
